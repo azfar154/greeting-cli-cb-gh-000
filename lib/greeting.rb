5b265914424785cefc8da7d1448a2027490a6a60
Azfar Mohamed <azfarmah@outlook.com>
2019-10-26 21:06:29 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-  return "Hello #{name}. It is nice to meet yourspec spec/02_cli_spec.rb"
+  return "Hello #{name}. It's nice to meet you.
 end
